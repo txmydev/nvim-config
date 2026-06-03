d637a94b4c84bc8f8816e441a30d916a26e5eed5
Tomas Mancini <tomi@mancini.es>
2026-06-03 00:01:23 -0300

    feat: upload new config

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,44 @@
+return {
+    {
+        "neovim/nvim-lspconfig",
+
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+        },
+
+        config = function()
+            require("mason").setup()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "pyright",
+                    "ts_ls",
+                },
+                automatic_installation = true
+            })
+
+            vim.lsp.config("lua_ls", {})
+            vim.lsp.config("pyright", {})
+            vim.lsp.config("ts_ls", {})
+            vim.lsp.config("jdtls", {})
+
+            vim.lsp.enable("jdtls")
+            vim.lsp.enable("lua_ls")
+            vim.lsp.enable("pyright")
+            vim.lsp.enable("ts_ls")
+
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(ev)
+                    local opts = { buffer = ev.buf }
+
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+                end,
+            })
+        end,
+    },
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,65 @@
+return {
+	{
+		{
+			'nvim-treesitter/nvim-treesitter',
+			lazy = false,
+			build = function()
+				local TS = require('nvim-treesitter')
+				if not TS.get_installed then
+					return
+				end
+			end,
+			cmds = { "TSUpdate", "TSInstall", "TSUninstall", "TSLog" },
+			opts_extend = { "ensure_installed " },
+			opts = {
+				indent = { enable = true },
+				highlight = { enable = true },
+				folds = { enable = true },
+				ensure_installed = {
+					"bash",
+					"c",
+					"diff",
+					"html",
+					"javascript",
+					"jsdoc",
+					"json",
+					"lua",
+					"luadoc",
+					"luap",
+					"markdown",
+					"markdown_inline",
+					"printf",
+					"python",
+					"query",
+					"regex",
+					"toml",
+					"tsx",
+					"typescript",
+					"vim",
+					"vimdoc",
+					"xml",
+					"yaml",
+				}
+			},
+			config = function(_, opts)
+                local TS = require('nvim-treesitter')
+                TS.setup(opts)
+
+                vim.diagnostic.config({
+                    virtual_text = true,
+                    signs = false
+                })
+
+                vim.api.nvim_create_autocmd('FileType', {
+                    pattern = { '<filetype>' },
+                    callback = function() vim.treesitter.start() end,
+                })
+
+                vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+                vim.wo[0][0].foldmethod = 'expr'
+
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
+		},
+	}
+}
