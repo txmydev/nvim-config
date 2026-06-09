61f6648e1228fb9b7b0126ccb5c5d5f548542a37
Tomas Mancini <tomi@mancini.es>
2026-06-09 14:52:49 -0300

    feat: add harpoon and change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,20 +1,27 @@
 return {
-    {
-        "catppuccin/nvim",
+    { "catppuccin/nvim",
         name = "catppuccin",
         enabled = true,
-        config = function()
+        opts = {
+            transparent_background = true,
+            no_italic = true,
+        },
+        config = function(_, opts)
+            require('catppuccin').setup(opts)
             vim.cmd("colorscheme catppuccin-macchiato")
         end,
     },
-    -- ROSE-PINE
+    -- GRUVBOX
     {
         "ellisonleao/gruvbox.nvim",
         enabled = false,
-        config = function()
+        opts = {
+            transparent_mode = true,
+        },
+        config = function(_, opts)
+            require('gruvbox').setup(opts)
             vim.cmd("colorscheme gruvbox")
         end,
-        opts = {}
     },
     -- ROSE-PINE
     {
@@ -29,6 +36,19 @@ return {
         config = function(_, opts)
             require('rose-pine').setup(opts)
             vim.cmd("colorscheme rose-pine")
+        end,
+    },
+    -- VAGUE
+    {
+        "https://github.com/vague-theme/vague.nvim",
+        name = "vague",
+        enabled = false,
+        opts = {
+            transparent = true,
+        },
+        config = function(_, opts)
+            require('vague').setup(opts)
+            vim.cmd("colorscheme vague")
         end,
     }
 }
