5708b32e552b15579a59429bdc80461074221640
Tomas Mancini <tomi@mancini.es>
2026-06-09 23:28:30 -0300

    other: add keymap to open float diagnostic

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,4 +3,5 @@ vim.keymap.set({ 'n' }, "<leader>ch", function() vim.fn.setreg('/', ""); end, { 
 vim.keymap.set("n", "<leader>tN", "<cmd>tabnew<cr>",    { desc = "New tab" })
 vim.keymap.set("n", "<leader>tn", "<cmd>tabnext<cr>",   { desc = "Next tab" })
 vim.keymap.set("n", "<leader>tc", "<cmd>tabclose<cr>",  { desc = "Close tab" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float,  { desc = "Show diagnostic float" })
 
