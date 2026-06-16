73be4e3b77f66ae0aaac85c7aa2d1c5dc4a56727
Tomas Mancini <tomi@mancini.es>
2026-06-16 12:37:05 -0300

    other: remap harpoon binds

--- lua/plugins/harpoon2.lua
+++ lua/plugins/harpoon2.lua
@@ -8,16 +8,16 @@ return {
 
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<A-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-        vim.keymap.set("n", "<A-h>", function() harpoon:list():next() end)
-        vim.keymap.set("n", "<A-g>", function() harpoon:list():prev() end)
-        vim.keymap.set("n", "<A-1>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<A-2>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<A-3>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<A-4>", function() harpoon:list():select(4) end)
-        vim.keymap.set("n", "<A-5>", function() harpoon:list():select(5) end)
-        vim.keymap.set("n", "<A-6>", function() harpoon:list():select(6) end)
-        vim.keymap.set("n", "<A-7>", function() harpoon:list():select(7) end)
-        vim.keymap.set("n", "<A-8>", function() harpoon:list():select(8) end)
+        vim.keymap.set("n", "<A-g>", function() harpoon:list():next() end)
+        vim.keymap.set("n", "<A-h>", function() harpoon:list():prev() end)
+        vim.keymap.set("n", "<A-d>", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<A-f>", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<A-g>", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<A-c>", function() harpoon:list():select(4) end)
+        vim.keymap.set("n", "<A-v>", function() harpoon:list():select(5) end)
+        -- vim.keymap.set("n", "<A-6>", function() harpoon:list():select(6) end)
+        -- vim.keymap.set("n", "<A-7>", function() harpoon:list():select(7) end)
+        -- vim.keymap.set("n", "<A-8>", function() harpoon:list():select(8) end)
 
     end
 }
